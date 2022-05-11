371ee7f41537940e67c7d863ed4b2162ed4cd7f1
agu <agu@indocresearch.org>
2022-05-11 11:57:17 -0400

    Add license

--- filecopy/tests/services/cataloguing/test_client.py
+++ filecopy/tests/services/cataloguing/test_client.py
@@ -1,3 +1,15 @@
+# Copyright (C) 2022 Indoc Research
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with this program.
+# If not, see http://www.gnu.org/licenses/.
+
 class TestCataloguingServiceClient:
     def test_create_catalog_entity_returns_geid(self, cataloguing_service_client, httpserver, fake, create_node):
         expected_guid = fake.geid()
--- filecopy/tests/services/metadata/test_client.py
+++ filecopy/tests/services/metadata/test_client.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2022 Indoc Research
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with this program.
+# If not, see http://www.gnu.org/licenses/.
+
 class TestMetadataServiceClient:
     def test_is_file_exists_returns_true_if_node_exists(self, metadata_service_client, mocker, create_node):
         mocker.patch.object(metadata_service_client, 'get_node', return_value=create_node())
